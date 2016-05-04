2c66ebb438df3c27c07e36677884cfdffc351ec2
Krzysztof Śmiałek <admin@avensome.net>
2016-05-04 17:30:06 +0200

    Falling shards

--- Assets/Scripts/MonoBehaviours/BreakablePlatform.cs
+++ Assets/Scripts/MonoBehaviours/BreakablePlatform.cs
@@ -3,14 +3,18 @@ using System.Collections;
 using System.Collections.Generic;
 
 [RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
 [RequireComponent(typeof(MeshCollider))]
 [RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(InertPlatform))]
 public class BreakablePlatform : ForceReceiver {
 
     public float fractureForceThreshold = 20;
-
     [Range(0, 1)]
     public float minSliceArea = 0.03f;
+    [Range(0.1f, 0.9f)]
+    public float shardDeathThreshold = 0.3f;
+    public Material deadMaterial;
 
     private Vector2[] vertices = null;
     private Rect aabb;
@@ -82,8 +86,8 @@ public class BreakablePlatform : ForceReceiver {
         // Perpendicular vector is normalized (length 1).
         // We can multiply it by AABB diagonal length, then add and subtract
         // from hit point to get segment points guaranteed to be outside the AABB.
-        Vector2 p1 = flatten(localHitPoint + aabbDiagonal.magnitude * perpendicular);
-        Vector2 p2 = flatten(localHitPoint + -aabbDiagonal.magnitude * perpendicular);
+        Vector2 p1 = removeYComponent(localHitPoint + aabbDiagonal.magnitude * perpendicular);
+        Vector2 p2 = removeYComponent(localHitPoint + -aabbDiagonal.magnitude * perpendicular);
 
         var slices = PolyKU.Slice(vertices, p1, p2);
         foreach (Vector2[] slice in slices) {
@@ -97,18 +101,34 @@ public class BreakablePlatform : ForceReceiver {
             GameObject shard = Instantiate(transform.gameObject) as GameObject;
             shard.name = "Platform Shard";
             shard.GetComponent<BreakablePlatform>().setShape(slice, lowY, highY);
+            float area = PolyU.GetArea(slice);
+            if (area < shardDeathThreshold) {
+                shard.GetComponent<BreakablePlatform>().kill();
+            }
         }
         Destroy(transform.gameObject);
     }
 
-    private static Vector2 flatten(Vector3 vector) {
+    private static Vector2 removeYComponent(Vector3 vector) {
         return new Vector2(vector.x, vector.z);
     }
 
-    public void setShape(Vector2[] vertices, float lowY, float highY) {
+    private void setShape(Vector2[] vertices, float lowY, float highY) {
         var mesh = Geometry.extrudePolygon(vertices, lowY, highY);
         GetComponent<MeshFilter>().mesh = mesh;
         GetComponent<MeshCollider>().sharedMesh = mesh;
         this.vertices = vertices;
+    }
+
+    private void kill() {
+        GetComponent<InertPlatform>().enabled = false;
+
+        if (deadMaterial != null) {
+            GetComponent<MeshRenderer>().material = deadMaterial;
+        }
+
+        var rigidbody = GetComponent<Rigidbody>();
+        rigidbody.constraints = RigidbodyConstraints.None;
+        rigidbody.useGravity = true;
     }
 }
