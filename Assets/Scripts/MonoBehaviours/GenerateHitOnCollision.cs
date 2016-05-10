23882cc2da9447186a7cd0a602e3af059997d427
Krzysztof Śmiałek <admin@avensome.net>
2016-05-10 11:42:52 +0200

    Calculate hit force from velocity delta

--- Assets/Scripts/MonoBehaviours/GenerateHitOnCollision.cs
+++ Assets/Scripts/MonoBehaviours/GenerateHitOnCollision.cs
@@ -22,11 +22,12 @@ public class GenerateHitOnCollision : MonoBehaviour {
 
     void OnControllerColliderHit(ControllerColliderHit hit) {
         ForceReceiver forceReceiver = hit.gameObject.GetComponentInChildren(typeof(ForceReceiver), false) as ForceReceiver;
+
         if (forceReceiver == null) {
             return;
         }
 
-        AppliedForce hitForce = new AppliedForce(hit.point, lastVelocity.magnitude * velocityToForceFactor);
+        AppliedForce hitForce = new AppliedForce(hit.point, (lastVelocity - hit.rigidbody.velocity).magnitude * velocityToForceFactor);
         forceReceiver.receiveHit(hitForce);
         lastVelocity = Vector3.zero;
     }
