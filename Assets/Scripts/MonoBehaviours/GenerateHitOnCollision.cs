e32e93b1148db0d3916a333a938c436f472eb28f
Krzysztof Śmiałek <admin@avensome.net>
2016-06-07 00:37:15 +0200

    Moving player with platforms

--- Assets/Scripts/MonoBehaviours/GenerateHitOnCollision.cs
+++ Assets/Scripts/MonoBehaviours/GenerateHitOnCollision.cs
@@ -6,20 +6,24 @@ using UnityStandardAssets.Characters.FirstPerson;
 [RequireComponent(typeof(Rigidbody))]
 public class GenerateHitOnCollision : MonoBehaviour {
 
-    public GameObject restartScreen; 
-    
+    public GameObject restartScreen;
+
     public float velocityToForceFactor = 1;
 
     public float durability = 40;
-    
+
     private Vector3 lastVelocity;
 
     private CharacterController characterController;
 
-    private ScoreCounter scoreCounter; 
-    
-    private Vector3 position; 
-    
+    private ScoreCounter scoreCounter;
+
+    private Vector3 position;
+
+    private Transform groundObject;
+
+    private Vector3 lastGroundPosition;
+
     void Start() {
         characterController = GetComponent<CharacterController>();
         scoreCounter = GetComponent<ScoreCounter>();
@@ -28,26 +32,36 @@ public class GenerateHitOnCollision : MonoBehaviour {
 
     void FixedUpdate() {
         lastVelocity = characterController.velocity;
+        if (!characterController.isGrounded) {
+            groundObject = null;
+        } else if (groundObject != null) {
+            var groundPositionDelta = groundObject.position - lastGroundPosition;
+            groundPositionDelta = new Vector3(groundPositionDelta.x, groundPositionDelta.y * 2, groundPositionDelta.z);
+            characterController.Move(groundPositionDelta);
+            lastGroundPosition = groundObject.position;
+        }
     }
 
     void OnControllerColliderHit(ControllerColliderHit hit) {
+        groundObject = hit.transform;
+        lastGroundPosition = groundObject.position;
+
         ForceReceiver forceReceiver = hit.gameObject.GetComponentInChildren(typeof(ForceReceiver), false) as ForceReceiver;
         if (forceReceiver == null) {
             return;
         }
 
         float forceMagnitude = (lastVelocity - hit.rigidbody.velocity).magnitude * velocityToForceFactor;
-        
+
         AppliedForce hitForce = new AppliedForce(hit.point, forceMagnitude);
         forceReceiver.receiveHit(hitForce);
-        
+
         if (forceMagnitude > durability) {
             restartScreen.gameObject.SetActive(true);
         } else {
             scoreCounter.updateScore();
         }
-        
+
         lastVelocity = Vector3.zero;
-        
     }
 }
